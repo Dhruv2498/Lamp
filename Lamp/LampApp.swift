b86981ada4ce95caa253cd963a16910b0fca22c7
Donda Dhruvit <145428600+Dhruv2498@users.noreply.github.com>
2024-02-19 16:31:55 +0530

    LamUi
    Its simple UI for lamp based on  its opacity using swiftUi

---
+++ Lamp/LampApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampApp.swift
+//  Lamp
+//
+//  Created by Dear D on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
